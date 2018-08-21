b5c067fc1ce9ab948a7849306ee64ffcaf18b126
Hoa Vo <whoait@gmail.com>
2018-08-21 21:47:11 +0700

    Example propTypes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+import PropTypes from 'prop-types';
 
 function Sum(props){
   return <h1> 
     {props.a} + {props.b} = {props.a + props.b }
   </h1>
 }
-ReactDOM.render(<Sum a = {'key'} b = {'board'} />, document.getElementById('root'));
+
+Sum.propTypes = {
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired
+}
+ReactDOM.render(<Sum a = {'key'} b = {'xx'} />, document.getElementById('root'));
 registerServiceWorker();
